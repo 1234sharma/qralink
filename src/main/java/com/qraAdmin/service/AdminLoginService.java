de6078738be82ff4811e742ff5d795d049e5a64a
Suraj Sharma <sharmasuraj95596@gmail.com>
2023-05-31 21:48:16 +0530

    solving login related issue

--- src/main/java/com/qraAdmin/service/AdminLoginService.java
+++ src/main/java/com/qraAdmin/service/AdminLoginService.java
@@ -6,15 +6,20 @@ import org.springframework.stereotype.Service;
 public class AdminLoginService {
 public String forpasswordvalidation(String username, String password)
 {
+	System.out.println("username"+username);
+	System.out.println("password"+password);
 	String usernameAdmin ="qralink@gmail.com";
 	String AdminuserPassword ="9044221797";
-	if(username.equals(usernameAdmin) || AdminuserPassword.equals(password) )
+	if(username.equals(usernameAdmin)==true && password.equals(AdminuserPassword))
 	{
+		System.out.println("username in side if"+username);
 		return "success";
 	}
 	else
 	{
-		return "Unsucccess";
+		System.out.println("username in side else"+username);
+		return "unsucess";
 	}
+	
 }
 }
